a01b4623bd638f6c1983c83860f1bf6ed44e853b
UMESH <159292843+UmeshReddy27@users.noreply.github.com>
2025-07-30 15:35:11 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -78,19 +78,18 @@ scrollToTopBtn.addEventListener('click', function() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 });
 // Hiding Header on Scroll
-        let lastScrollTop = 0;
-        const header = document.getElementById('header');
-        const headerHeight = header.offsetHeight;
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+const headerHeight = header.offsetHeight;
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        window.addEventListener('scroll', function() {
-            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-            if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
+    if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
                 // Scrolling Down
-                header.classList.add('-translate-y-full');
-            } else {
+        header.classList.add('-translate-y-full');
+    } else {
                 // Scrolling Up
-                header.classList.remove('-translate-y-full');
-            }
-            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-        }, false);
+        header.classList.remove('-translate-y-full');
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}, false);
